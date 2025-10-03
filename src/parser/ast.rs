7ba70a456d61c90c0a0e695debcde3695b2933c5
Nignik <maksymilian.olbrys@gmail.com>
2025-10-03 19:03:56 +0200

    feat: grammar definition

--- src/parser/ast.rs
+++ src/parser/ast.rs
@@ -1,5 +1,17 @@
 use crate::lexer::token::Token;
 
+/*
+expression     → equality ;
+equality       → comparison ( ( "!=" | "==" ) comparison )* ;
+comparison     → term ( ( ">" | ">=" | "<" | "<=" ) term )* ;
+term           → factor ( ( "-" | "+" ) factor )* ;
+factor         → unary ( ( "/" | "*" ) unary )* ;
+unary          → ( "!" | "-" ) unary
+               | primary ;
+primary        → NUMBER | STRING | "true" | "false" | "nil"
+               | "(" expression ")" ;
+*/
+
 pub enum Expr {
   Binary { left: Box<Expr>, right: Box<Expr>, operator: Token },
   Grouping { expr: Box<Expr> },
