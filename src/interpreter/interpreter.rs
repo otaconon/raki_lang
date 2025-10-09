077ac1adc19b07afd3c0aace3d0a5a581d099402
Nignik <maksymilian.olbrys@gmail.com>
2025-10-09 16:51:55 +0200

    feat: nothing

--- src/interpreter/interpreter.rs
+++ src/interpreter/interpreter.rs
@@ -18,54 +18,68 @@ impl Interpreter {
   }
 }
 
+impl Interpreter {
+  fn visit_binary_expr(&self, left: &Expr, right: &Expr, operator: &Token) -> Result<Object, RakiError> {
+    let left: Object = self.visit_expr(left)?;
+    let right: Object = self.visit_expr(right)?;
+
+    let (left, right) = match (left, right) {
+      (Object::Double(l), Object::Double(r)) => (l, r),
+      _ => return Err(RakiError::Runtime {}),
+    };
+
+    match operator.r#type {
+      TokenType::Plus => return Ok(Object::Double(left + right)), // TODO: overload '+' operator for strings
+      TokenType::Minus => return Ok(Object::Double(left - right)),
+      TokenType::Star => return Ok(Object::Double(left * right)),
+      TokenType::Slash => return Ok(Object::Double(left / right)),
+      TokenType::Greater => return Ok(Object::Boolean(left > right)),
+      TokenType::GreaterEqual => return Ok(Object::Boolean(left >= right)),
+      TokenType::Less => return Ok(Object::Boolean(left < right)),
+      TokenType::LessEqual => return Ok(Object::Boolean(left <= right)),
+      TokenType::BangEqual => return Ok(Object::Boolean(left != right)), // TODO: abstract != and == handling to a fucntion that handles other values than Double
+      TokenType::EqualEqual => return Ok(Object::Boolean(left == right)),
+      _ => return Ok(Object::None),
+    }
+  }
+
+  fn visit_literal_expr(&self, lit: &LiteralType) -> Result<Object, RakiError> {
+    match lit {
+      LiteralType::String(s) => return Ok(Object::String(s.clone())),
+      LiteralType::I64(val) | LiteralType::F64(val) => return Ok(Object::Double(*val)),
+      LiteralType::Bool(val) => return Ok(Object::Boolean(*val)),
+      LiteralType::None => return Ok(Object::None),
+    }
+  }
+
+  fn visit_unary_expr(&self, right: &Expr, operator: &Token) -> Result<Object, RakiError> {
+    let right = self.visit_expr(right)?;
+    match operator.r#type {
+      TokenType::Minus => match right {
+        Object::Double(val) => return Ok(Object::Double(-val)),
+        _ => return Err(RakiError::Runtime {}),
+      },
+      TokenType::Bang => match right {
+        Object::Boolean(val) => return Ok(Object::Boolean(!val)),
+        Object::None => return Ok(Object::Boolean(false)),
+        _ => return Err(RakiError::Runtime {}),
+      },
+      _ => return Ok(Object::None),
+    }
+  }
+
+  fn visit_ternary_expr(&self, condition: &Expr, left: &Expr, right: &Expr) -> Result<Object, RakiError> {
+    
+  }
+}
+
 impl Visitor<Result<Object, RakiError>> for Interpreter {
   fn visit_expr(&self, e: &Expr) -> Result<Object, RakiError> {
     match e {
-      Expr::Binary { left, right, operator } => {
-        let left: Object = self.visit_expr(left)?;
-        let right: Object = self.visit_expr(right)?;
-
-        let (left, right) = match (left, right) {
-          (Object::Double(l), Object::Double(r)) => (l, r),
-          _ => return Err(RakiError::Runtime {  })
-        };
-        
-        match operator.r#type {
-          TokenType::Plus => return Ok(Object::Double(left + right)), // TODO: overload '+' operator for strings
-          TokenType::Minus => return Ok(Object::Double(left - right)),
-          TokenType::Star => return Ok(Object::Double(left * right)),
-          TokenType::Slash => return Ok(Object::Double(left / right)),
-          TokenType::Greater => return Ok(Object::Boolean(left > right)),
-          TokenType::GreaterEqual => return Ok(Object::Boolean(left >= right)),
-          TokenType::Less => return Ok(Object::Boolean(left < right)),
-          TokenType::LessEqual => return Ok(Object::Boolean(left <= right)),
-          TokenType::BangEqual => return Ok(Object::Boolean(left != right)), // TODO: abstract != and == handling to a fucntion that handles other values than Double
-          TokenType::EqualEqual => return Ok(Object::Boolean(left == right)),
-          _ => return Ok(Object::None)
-        }
-      },
+      Expr::Binary { left, right, operator } => return self.visit_binary_expr(left, right, operator),
       Expr::Grouping { expr } => return self.visit_expr(&expr),
-      Expr::Literal { value } => match value {
-        LiteralType::String(s) => return Ok(Object::String(s.clone())),
-        LiteralType::I64(val) | LiteralType::F64(val) => return Ok(Object::Double(*val)),
-        LiteralType::Bool(val) => return Ok(Object::Boolean(*val)),
-        LiteralType::None => return Ok(Object::None),
-      },
-      Expr::Unary { right, operator } => {
-        let right = self.visit_expr(right)?;
-        match operator.r#type {
-          TokenType::Minus => match right {
-            Object::Double(val) => return Ok(Object::Double(-val)),
-            _ => return Err(RakiError::Runtime {}),
-          },
-          TokenType::Bang => match right {
-            Object::Boolean(val) => return Ok(Object::Boolean(!val)),
-            Object::None => return Ok(Object::Boolean(false)),
-            _ => return Err(RakiError::Runtime {}),
-          },
-          _ => return Ok(Object::None),
-        }
-      }
+      Expr::Literal { value } => return self.visit_literal_expr(value),
+      Expr::Unary { right, operator } => return self.visit_unary_expr(right, operator),
       Expr::Ternary { condition, left, right } => return Ok(Object::None),
     }
   }
